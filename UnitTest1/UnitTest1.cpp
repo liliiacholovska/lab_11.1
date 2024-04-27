dd42df4151e7238c5e8e5d330d29094735c2097c
Liliia Cholovska <145839750+liliiacholovska@users.noreply.github.com>
2024-04-27 10:43:53 +0300

    Update UnitTest1.cpp

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "CppUnitTest.h"
 #include "../Lab11.1/lab11.1.cpp"
 
@@ -10,39 +10,33 @@ namespace UnitTest1
 	{
 	public:
 
-        TEST_METHOD(TestProcessData) {
-            const char* test_input_file = "t1.txt";
-            const char* test_output_file = "t2.txt";
-            ofstream testInput(test_input_file);
-            testInput << "1 2 3 4 5";
+        TEST_METHOD(TestCalculateAverages) {
+            const char* test_input_file = "t1.bin";
+            ofstream testInput(test_input_file, ios::binary);
+            int numbers[] = { 1, 2, 3, 4, 5 };
+            for (int num : numbers) {
+                testInput.write(reinterpret_cast<char*>(&num), sizeof(num));
+            }
             testInput.close();
 
-            ProcessData(test_input_file, test_output_file);
+            double avgMinMax, overallAvg;
+            CalculateAverages(test_input_file, avgMinMax, overallAvg);
 
-            ifstream testOutput(test_output_file);
-            stringstream buffer;
-            buffer << testOutput.rdbuf();
-            string outputContent = buffer.str();
-
-            string expectedOutput = "Average of max and min components: 3\nAverage of all components: 3\n";
-            Assert::AreEqual(expectedOutput, outputContent);
+            Assert::AreEqual(3.0, avgMinMax, 0.01); 
+            Assert::AreEqual(3.0, overallAvg, 0.01); 
         }
 
-        TEST_METHOD(TestPrintFileContents) {
-            const char* test_input_file = "t.txt";
-            ofstream testInput(test_input_file);
-            string fileData = "Line 1\nLine 2\nLine 3";
-            testInput << fileData;
-            testInput.close();
+        TEST_METHOD(TestWriteAndReadAverages) {
+            const char* test_output_file = "t2.bin";
+            double avgMinMax = 3.0, overallAvg = 3.0;
 
-            streambuf* origCoutStreamBuf = std::cout.rdbuf();
-            ostringstream strCout;
-            cout.rdbuf(strCout.rdbuf());
-            PrintFileContents(test_input_file);
-            cout.rdbuf(origCoutStreamBuf);
-            string expectedOutput = "Contents of t.txt:\nLine 1\nLine 2\nLine 3\n";
+            WriteAveragesToFile(test_output_file, avgMinMax, overallAvg);
 
-            Assert::AreEqual(expectedOutput, strCout.str());
+            double readAvgMinMax, readOverallAvg;
+            ReadAveragesFromFile(test_output_file, readAvgMinMax, readOverallAvg);
+
+            Assert::AreEqual(avgMinMax, readAvgMinMax, 0.01);
+            Assert::AreEqual(overallAvg, readOverallAvg, 0.01);
         }
 	};
 }
